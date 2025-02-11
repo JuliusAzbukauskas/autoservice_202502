95aa44f746ad9a78cea1f2f54d84bd563f542e34
Julius Azbukauskas <ajulius89@gmail.com>
2025-02-11 19:57:54 +0200

    aprašau modelį admin admin http://127.0.0.1:8000/

--- mysite/autoservice/admin.py
+++ mysite/autoservice/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from.models import (Service,
+                    CarModel,
+                    Car,
+                    Order)
 
 # Register your models here.
+admin.site.register(Service)
+admin.site.register(CarModel)
+admin.site.register(Car)
+admin.site.register(Order)
+
